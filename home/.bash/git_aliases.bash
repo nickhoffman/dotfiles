46a16c0f45574d463e4653d72ac7682569c3dcfb
Nick Hoffman <nick@deadorange.com>
2014-06-27 10:49:46 -0400

    Add the "gcob" Git alias.

--- home/.bash/git_aliases.bash
+++ home/.bash/git_aliases.bash
@@ -36,6 +36,7 @@ if [ "x$?" = "x0" ]; then
   __git_shortcut gb     branch
   __git_shortcut gba    branch -a
   __git_shortcut gco    checkout
+  __git_shortcut gcob   checkout -b
   __git_shortcut gcom   checkout master
   __git_shortcut grm    rebase master
   __git_shortcut gp     push
