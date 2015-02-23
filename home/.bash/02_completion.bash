708d2b82e0c09f81ee1040411ac5ebd1afb6b8ab
Nick Hoffman <nick@deadorange.com>
2015-02-23 10:15:37 -0500

    Enable extblog in Bash.

--- home/.bash/02_completion.bash
+++ home/.bash/02_completion.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "`uname -s`" = "Darwin" ]; then
+  # Needed for The Silver Searcher's Bash completion.
+  shopt -s extglob
+fi
+
 if [[ -d /usr/local/etc/bash_completion.d ]]; then
   for file in /usr/local/etc/bash_completion.d/*; do
     source $file
