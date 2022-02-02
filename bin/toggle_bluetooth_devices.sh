fc97f254a4ef315ce932306219b79c2048e68c47
Nick Hoffman <nhoffman@bolt.com>
2022-02-01 21:17:51 -0500

    Add a script for toggling Bluetooth and Bluetooth devices.

---
+++ bin/toggle_bluetooth_devices.sh
@@ -0,0 +1,22 @@
+#!/usr/local/bin/zsh
+
+# Bluetooth device IDs
+trackpad_id='d8-96-95-e1-c8-08'   # Nick Hoffman's Trackpad
+keyboard_id='c8-e0-eb-0f-a0-9a'   # Nick Hoffman’s Keyboard
+
+bt_cmd='/usr/local/bin/blueutil'
+
+$bt_cmd --power | grep -q '^1$'
+if [[ "x$?" = 'x0' ]]; then
+  # Disconnect the devices and turn off Bluetooth
+  $bt_cmd --disconnect "$trackpad_id"
+  $bt_cmd --disconnect "$keyboard_id"
+  $bt_cmd --power 0
+  say 'disconnected'
+else
+  # Turn on Bluetooth and connect the devices
+  $bt_cmd --power 1
+  $bt_cmd --connect "$trackpad_id"
+  $bt_cmd --connect "$keyboard_id"
+  say 'connected'
+fi
