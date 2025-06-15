99ca8b50548cbd8843584f4d0e46d459a8620827
Nick Hoffman <nick@deadorange.com>
2025-06-15 12:14:32 -0400

    Configure Neovim to have separate keymappings for all sessions vs terminal sessions.

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -2,13 +2,15 @@
 vim.opt.ignorecase = true
 -- END: Move these configs to separate file.
 
+require "keymappings"
+
 if vim.g.vscode then
   -- VSCode Neovim
   require "vscode_keymappings"
 else
   -- Ordinary Neovim
   vim.wo.number = true
-  require "keymappings"
+  require "terminal_keymappings"
 
   -- Load the config from ~/.config/vimrc.vim
   local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
--- home/.config/nvim/lua/keymappings.lua
+++ home/.config/nvim/lua/keymappings.lua
@@ -1,13 +1,5 @@
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
--- Map '<' to switch to the previous tab, and '>' to switch to the next tab.
-keymap("n", "<", "gT", opts)
-keymap("n", ">", "gt", opts)
-
 -- Map ';' to swap the position of the current line and the one below it.
 keymap("n", ";", "ddpk0", opts)
-
--- Make "j" and "k" move through wrapped lines.
-keymap({"n", "v"}, "j", "gj", opts)
-keymap({"n", "v"}, "k", "gk", opts)
